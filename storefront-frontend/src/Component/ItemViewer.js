71163edfff2f0e03fe6e1a99b0ece97b97e0145a
Jack Arthur Harrhy <me@jackharrhy.com>
2020-06-20 12:47:45 -0230

    parse internal nbt data, display in json dump

--- storefront-frontend/src/Component/ItemViewer.js
+++ storefront-frontend/src/Component/ItemViewer.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import JSONPretty from 'react-json-pretty';
+import nbtParser from 'prismarine-nbt';
+import { Buffer } from 'buffer';
+
 import 'react-json-pretty/themes/monikai.css';
 
 import Item from '../Component/Item';
@@ -7,20 +10,48 @@ import Item from '../Component/Item';
 // TODO nice non-json view of most data, with json view always there but more hidden
 const showJson = true;
 
-export default ({ currentItem, clearCurrentItem }) => {
+function base64ToBuffer(base64) {
+	const binaryString = atob(base64);
+	const len = binaryString.length;
+	const bytes = new Uint8Array(len);
+	for (let i = 0; i < len; i++) {
+		bytes[i] = binaryString.charCodeAt(i);
+	}
+	return Buffer.from(bytes.buffer);
+}
+
+export default ({ currentItem: item, clearCurrentItem }) => {
+	const [nbt, setNbt] = useState(null);
+
+	useEffect(() => {
+		if (item.meta?.internal) {
+			const buf = base64ToBuffer(item.meta.internal);
+			nbtParser.parse(buf, (error, result) => {
+				if (error) {
+					console.error(error);
+				} else {
+					setNbt(result);
+				}
+			});
+		}
+	}, [item]);
+
 	return (
 		<div id="item-viewer">
 			<div>
 				<Item
-					item={currentItem}
+					item={item}
 					setCurrentItem={() => {}}
 				/>
 				{showJson && (
 					<div className="json">
-						<JSONPretty data={currentItem}></JSONPretty>
+						<JSONPretty data={{
+							item,
+							nbt,
+						}}/>
 					</div>
 				)}
-				<p>{currentItem.name}</p>
+				<p>{item.name}</p>
 				<button className="close" onClick={clearCurrentItem}>×</button>
 			</div>
 		</div>
